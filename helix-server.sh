d703d2d5572ddf17d58c309355d049df2d81e787
Kai Davenport <kaiyadavenport@gmail.com>
2024-04-17 10:35:27 +0100

    default for openai key

--- helix-server.sh
+++ helix-server.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 export IMAGE=${IMAGE:="europe-docker.pkg.dev/helixml/helix/controlplane:latest"}
+export OPENAI_API_KEY=${OPENAI_API_KEY:=""}
 
 if [[ -z $OPENAI_API_KEY ]]; then
   echo "OPENAI_API_KEY is empty. Exiting..." >&2
